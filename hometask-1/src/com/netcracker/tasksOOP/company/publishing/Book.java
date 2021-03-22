e37824ab363b1a46a35370c6649d41f556283664
Python3xsssss <biathloner2@mail.ru>
2021-03-22 13:14:36 +0300

    Initial commit

---
+++ hometask-1/src/com/netcracker/tasksOOP/company/publishing/Book.java
@@ -0,0 +1,18 @@
+package com.netcracker.tasksOOP.company.publishing;
+
+public class Book {
+
+    class Author {
+        private String name;
+        private String email;
+        private char gender;
+    }
+
+    private String name;
+    private Author[] authors;
+    private double price;
+    private int qty = 0;
+
+
+
+}
